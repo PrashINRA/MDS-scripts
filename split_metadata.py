d1ec3c57c8e99eb38fb929a3f7c056d6b94c522a
Marwaafe <58457914+Marwaafe@users.noreply.github.com>
2025-02-27 11:10:12 +0100

    Update and rename Splitting Metadata to split_metadata.py

--- split_metadata.py
+++ split_metadata.py
@@ -2,44 +2,31 @@ mkdir -p /net/beegfs/scratch/mafechkar/MDS_Data/metadata
 
 
 nano /net/beegfs/scratch/mafechkar/MDS_Data/metadata/split_metadata.py
-#!/usr/bin/env python3
+
 import pandas as pd
 
 metadata_file = "/net/beegfs/scratch/mafechkar/MDS_Data/metadata/Data for scRNA seq analysis Updated.xlsx"
 
-# Read the Excel file:
+# Reading the Excel file:
 # header=1 for second row as the header,
 # sheet_name=cDNA& library prep uses the second sheet.
-metadata = pd.read_excel(metadata_file, engine='openpyxl', header=1, sheet_name=cDNA& library pre)
+metadata = pd.read_excel(metadata_file, engine='openpyxl', header=1, sheet_name="cDNA& library pre")
 
-# For GEX: extract the columns "MDS#" and "Index for GEX library"
-# and rename them to "Sample" and "Index" respectively.
+# For GEX: I extract the columns "MDS#" and "Index for GEX library"
+# and renaming them to "Sample" and "Index" respectively.
 gex_samples = metadata[['MDS#', 'Index for GEX library']].copy()
 gex_samples.rename(columns={'MDS#': 'Sample', 'Index for GEX library': 'Index'}, inplace=True)
-# Add a Lane column with a default value (e.g., "*")
+# Adding a Lane column with  default value (e.g., "*")
 gex_samples.insert(0, 'Lane', '*')
 
-# For Protein: extract the columns "MDS#" and "Index ab (Protein) library"
+# For Protein: I extract the columns "MDS#" and "Index ab (Protein) library"
 protein_samples = metadata[['MDS#', 'Index ab (Protein) library']].copy()
 protein_samples.rename(columns={'MDS#': 'Sample', 'Index ab (Protein) library': 'Index'}, inplace=True)
 protein_samples.insert(0, 'Lane', '*')
 
-# Save the two sample sheets as CSV files in your working directory on the cluster.
 gex_samples.to_csv('/net/beegfs/scratch/mafechkar/gex_samples.csv', index=False)
 protein_samples.to_csv('/net/beegfs/scratch/mafechkar/protein_samples.csv', index=False)
 
 print("Successfully split metadata into gex_samples.csv and protein_samples.csv")
 
 
-export PATH=~/miniconda3/bin:$PATH
-source ~/miniconda3/etc/profile.d/conda.sh
-
-
-python3 /net/beegfs/scratch/mafechkar/split_metadata.py
-ls -lh /net/beegfs/scratch/mafechkar/gex_samples.csv
-ls -lh /net/beegfs/scratch/mafechkar/protein_samples.csv
-head /net/beegfs/scratch/mafechkar/gex_samples.csv
-head /net/beegfs/scratch/mafechkar/protein_samples.csv
-
-
-MDS006-08-249_S12_L001_R1_001.fastq.gz
