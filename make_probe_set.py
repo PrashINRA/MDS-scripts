8c8e01c9ee88f0d7eb7143ed5b926382616bd387
Marwaafe <58457914+Marwaafe@users.noreply.github.com>
2025-03-04 14:35:57 +0100

    Update make_probe_set.py

--- make_probe_set.py
+++ make_probe_set.py
@@ -22,3 +22,29 @@ with open(output_csv, "w") as f:
 print(f"Probe set CSV file has been created at {probe_set_output}")
 
 
+t Tue Mar  4 14:33:59 CET 2025...
+
+
+Martian Runtime - v4.0.13
+2025-03-04 14:34:00 [jobmngr] WARNING: User-supplied amount 300 GB is higher than the detected cgroup memory limit of 40.0 GB
+Serving UI at http://node010.cluster:36535?auth=jOfm5QhPllTiquEoxyEE50yueVrbTe4mLrx1Y8iNFwM
+
+Running preflight checks (please wait)...
+
+[error] Pipestance failed. Error log at:
+MDS_Output/SC_MULTI_CS/MULTI_PREFLIGHT/fork0/chnk0-u2462c70148/_errors
+
+Log message:
+The following metadata fields are required in the probe set CSV header: "panel_name", "panel_type", "reference_genome", "reference_version", but were not found. Please include these fields in #field=value format at the top of the file.
+
+Waiting 6 seconds for UI to do final refresh.
+Pipestance failed. Use --noexit option to keep UI running after failure.
+
+2025-03-04 14:34:24 Shutting down.
+Saving pipestance info to "MDS_Output/MDS_Output.mri.tgz"
+For assistance, upload this file to 10x Genomics by running:
+
+cellranger upload <your_email> "MDS_Output/MDS_Output.mri.tgz"
+
+Cell Ranger Multi finished at Tue Mar  4 14:34:24 CET 2025. Output is in: /net/beegfs/scratch/mafechkar/MDS_Data/MDS_Output
+[mafechkar@ares mafechkar]$
