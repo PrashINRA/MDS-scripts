ebecec01c134c5b45746d2a64df40659763558d8
Marwaafe <58457914+Marwaafe@users.noreply.github.com>
2025-03-05 12:01:34 +0100

    Update make_probe_set.py

--- make_probe_set.py
+++ make_probe_set.py
@@ -21,35 +21,3 @@ with open(output_probe_csv, "w") as f:
 
 print(f"Probe set CSV file has been created at {output_probe_csv}")
 
-///
-
- cat cellranger_multi_counts.out
-Starting Cell Ranger Multi at Tue Mar  4 15:20:30 CET 2025...
-
-
-Martian Runtime - v4.0.13
-2025-03-04 15:20:30 [jobmngr] WARNING: User-supplied amount 300 GB is higher than the detected cgroup memory limit of 40.0 GB
-Serving UI at http://node001.cluster:36595?auth=TZp9aw8yyH2NL47iDslg9n-w2jN_jrcADQWZReGro60
-
-Running preflight checks (please wait)...
-
-[error] Pipestance failed. Error log at:
-MDS_Output/SC_MULTI_CS/MULTI_PREFLIGHT/fork0/chnk0-u7593c70c2e/_errors
-
-Log message:
-The feature reference file header does not contain one or more required comma-separated fields: "id, name, read, pattern, sequence, feature_type".
-The following fields were found: "".
-Please check that your file is in CSV format and has the required field names.
-
-Waiting 6 seconds for UI to do final refresh.
-Pipestance failed. Use --noexit option to keep UI running after failure.
-
-2025-03-04 15:20:45 Shutting down.
-Saving pipestance info to "MDS_Output/MDS_Output.mri.tgz"
-For assistance, upload this file to 10x Genomics by running:
-
-cellranger upload <your_email> "MDS_Output/MDS_Output.mri.tgz"
-
-Cell Ranger Multi finished at Tue Mar  4 15:20:46 CET 2025. Output is in: /net/beegfs/scratch/mafechkar/MDS_Data/MDS_Output
-
-
